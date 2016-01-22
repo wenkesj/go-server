32faf0070419e7d9ae2a42ca8d7c580a17aa3337
Sam Wenke <samwenke@gmail.com>
2016-01-22 12:40:56 -0500

    [src] Fix prod config

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@ sudo add-apt-repository ppa:duh/golang
 sudo apt-get update
 sudo apt-get install golang
 sudo apt-get install git
+export GOROOT=/usr/lib/go
+export GOBIN=/usr/bin/go
+mkdir lib
+export GOPATH=$HOME/lib
+mkdir lib/src
+mkdir lib/src/github.com
+mkdir lib/src/github.com/wenkesj
+git clone https://github.com/wenkesj/go-server.git lib/src/github.com/wenkesj/go-server
+go get github.com/wenkesj/go-server
